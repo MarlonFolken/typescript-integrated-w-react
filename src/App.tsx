5aad94112a6ff570617f50b5ce90bb3cff02278c
marlonfolken <marlonvargasmx@gmail.com>
2024-06-17 20:46:33 -0600

    Ejemplo creacion de types (ButtonProps)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import 'bulma/css/bulma.css';
+import Button from './components/Button';
 import './App.css';
 
 function App() {
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault()
+    console.log('Button clicked');
+  }
   return (
     <div className="App">
-      <button className="button is-primary">Hola Mundo</button>
+      <Button 
+        handleClick={handleClick}
+      >
+        Accionable
+      </Button>
     </div>
   );
 }
