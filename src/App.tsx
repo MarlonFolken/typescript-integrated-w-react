bd82732cbed31f79c5779d4674c3d22bdc05192e
marlonfolken <marlonvargasmx@gmail.com>
2024-06-17 21:19:05 -0600

    Tipado con Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React, { MouseEventHandler } from 'react';
+import React, { ChangeEventHandler, MouseEventHandler } from 'react';
 import 'bulma/css/bulma.css';
 import Button from './components/Button';
+import Input from './components/Input';
 import './App.css';
 
 function App() {
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    console.log(e.target.value);
+  }
   const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault()
     console.log('Button clicked');
   }
   return (
     <div className="App">
+      <Input placeholder='Nombre' handleChange={handleChange}/>
       <Button 
         handleClick={handleClick}
       >
